597faf4a3ec5c9bfe78a3b5f5b64c5cf6bff4195
pvashooot <pvashooot@rambler.ru>
2023-09-08 16:03:02 +0300

    Подключил себя к верстке

--- include/template/head.php
+++ include/template/head.php
@@ -11,3 +11,4 @@
 <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
 <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css"/>
 <link rel="stylesheet" href="/design/css/style.css">
+<link rel="stylesheet" href="/design/css/pvashooot.css">
