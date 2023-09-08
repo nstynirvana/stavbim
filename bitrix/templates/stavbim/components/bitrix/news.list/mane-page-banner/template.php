bb9662f35593fdc0a3c3be8138936733418ee62b
pvashooot <pvashooot@rambler.ru>
2023-09-08 13:18:42 +0300

    Баннер главный - главная странице - отключена возможность вывода постранички

--- bitrix/templates/stavbim/components/bitrix/news.list/mane-page-banner/template.php
+++ bitrix/templates/stavbim/components/bitrix/news.list/mane-page-banner/template.php
@@ -1,20 +1,6 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
-/** @var array $arParams */
-/** @var array $arResult */
-/** @global CMain $APPLICATION */
-/** @global CUser $USER */
-/** @global CDatabase $DB */
-/** @var CBitrixComponentTemplate $this */
-/** @var string $templateName */
-/** @var string $templateFile */
-/** @var string $templateFolder */
-/** @var string $componentPath */
-/** @var CBitrixComponent $component */
 $this->setFrameMode(true);
 ?>
-<?if($arParams["DISPLAY_TOP_PAGER"]):?>
-	<?=$arResult["NAV_STRING"]?><br />
-<?endif;?>
 <?foreach($arResult["ITEMS"] as $arItem):?>
 	<?
 	$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
@@ -26,8 +12,4 @@ $this->setFrameMode(true);
             <?=$arItem["DETAIL_TEXT"]?>
         </div>
     </section>
-
 <?endforeach;?>
-<?if($arParams["DISPLAY_BOTTOM_PAGER"]):?>
-	<br /><?=$arResult["NAV_STRING"]?>
-<?endif;?>
