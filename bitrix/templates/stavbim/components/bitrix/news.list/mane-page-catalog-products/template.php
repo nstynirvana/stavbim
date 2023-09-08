047f8bd9bf10e00ff7adba80f753722e5f2be35b
pvashooot <pvashooot@rambler.ru>
2023-09-08 15:52:08 +0300

    Список товаров на главной странице (МОИ РАБОТЫ)

--- bitrix/templates/stavbim/components/bitrix/news.list/mane-page-catalog-products/template.php
+++ bitrix/templates/stavbim/components/bitrix/news.list/mane-page-catalog-products/template.php
@@ -1,21 +1,6 @@
 <? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
-/** @var array $arParams */
-/** @var array $arResult */
-/** @global CMain $APPLICATION */
-/** @global CUser $USER */
-/** @global CDatabase $DB */
-/** @var CBitrixComponentTemplate $this */
-/** @var string $templateName */
-/** @var string $templateFile */
-/** @var string $templateFolder */
-/** @var string $componentPath */
-/** @var CBitrixComponent $component */
 $this->setFrameMode(true);
 ?>
-<? if ($arParams["DISPLAY_TOP_PAGER"]): ?>
-    <?= $arResult["NAV_STRING"] ?><br/>
-<? endif; ?>
-
 <script>
     window.addEventListener('DOMContentLoaded', () => {
         $('.works__slider').slick({
@@ -27,17 +12,15 @@ $this->setFrameMode(true);
             arrows: false,
             dotsClass: 'slick-dots brands__dots',
         });
-    });</script>
+    });
+</script>
 
 <section class="brands">
-
     <div class="container">
         <div class="works__title-wrapper">
             <h3 class="works__title">Готовые работы</h3>
             <a href="/my-works/" class="works__link">
-                <p>
-                    Смотреть все
-                </p>
+                <p>Смотреть все</p>
                 <svg width="17" height="14" viewBox="0 0 17 14" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M0 7H16M16 7L10 1M16 7L10 13" stroke="currentColor"/>
                 </svg>
@@ -51,41 +34,45 @@ $this->setFrameMode(true);
                 $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
                 ?>
 
-<!--                --><?//dp($arItem["PROPERTIES"]["PRICE"]["VALUE"]);?>
+                <?$sectionID = $arItem["IBLOCK_SECTION_ID"];?>
 
                 <div class="works__card" id="<?= $this->GetEditAreaId($arItem['ID']); ?>">
-                    <a href="<?= $arItem["LINK_PRODUCT"] ?>" class="works__img-container">
+                    <a href="<?=$arItem["DETAIL_PAGE_URL"]?>" class="works__img-container">
                         <?$arFile_1 = CFile::GetFileArray($arItem["PROPERTIES"]["PICTURE_DESKTOP"]["VALUE"]);?>
-                        <?$arFile_2 = CFile::GetFileArray($arItem["PROPERTIES"]["PICTURE_FILE"]["VALUE"]);?>
                         <img src="<?=$arFile_1["SRC"]?>" class="works__img" alt="">
-                        <img src="<?=$arFile_2["SRC"]?>" class="works__icon" alt="">
-                        <a href="<?= $arItem["LINK_PRODUCT"] ?>" class="works__card-modal">Модель для проектирования</a>
-                        <a href="<?= $arItem["LINK_PRODUCT"] ?>" class="works__card-descr"><?=$arItem["NAME"]?></a>
-                        <a href="<?= $arItem["LINK_PRODUCT"] ?>" class="works__card-price"><?=$arItem["PROPERTIES"]["PRICE"]["VALUE"]?></a>
-                        <button class="works__card-addToBasket">Добавить модель в корзину</button>
+                        <?if($arItem["PROPERTIES"]["PDF_FILE"]["VALUE"]):?>
+                            <img src="/design/icons/works_icon.svg" class="works__icon" alt="">
+                        <?endif;?>
+                    </a>
+                    <a href="<?=$arItem["DETAIL_PAGE_URL"]?>" class="works__card-modal"><?=$arResult["SECTIONS_LIST"][$sectionID]?></a>
+                    <a href="<?=$arItem["DETAIL_PAGE_URL"]?>" class="works__card-descr"><?=$arItem["NAME"]?></a>
+                    <a href="<?=$arItem["DETAIL_PAGE_URL"]?>" class="works__card-price"><?=number_format($arItem["PROPERTIES"]["PRICE"]["VALUE"], 0, ',', ' ')?> ₽</a>
+                    <button class="works__card-addToBasket add2Cart" data-itemid="<?=$arItem["ID"]?>" data-price="<?=$arItem["PROPERTIES"]["PRICE"]["VALUE"]?>">Добавить модель в корзину</button>
                 </div>
             <? endforeach; ?>
         </div>
 
         <div class="works__slider">
             <? foreach ($arResult["ITEMS"] as $arItem): ?>
+
+                <?$sectionID = $arItem["IBLOCK_SECTION_ID"];?>
+
                 <div class="works__slide">
                     <div class="works__card" id="<?= $this->GetEditAreaId($arItem['ID']); ?>">
-                        <a href="<?= $arItem["DETAIL_PAGE_URL"] ?>" class="works__img-container">
+                        <a href="<?=$arItem["DETAIL_PAGE_URL"]?>" class="works__img-container">
                             <?$arFile_1 = CFile::GetFileArray($arItem["PROPERTIES"]["PICTURE_DESKTOP"]["VALUE"]);?>
-                            <?$arFile_2 = CFile::GetFileArray($arItem["PROPERTIES"]["PICTURE_FILE"]["VALUE"]);?>
                             <img src="<?=$arFile_1["SRC"]?>" class="works__img" alt="">
-                            <img src="<?=$arFile_2["SRC"]?>" class="works__icon" alt="">
-                            <a href="<?= $arItem["LINK_PRODUCT"] ?>" class="works__card-modal">Модель для проектирования</a>
-                            <a href="<?= $arItem["LINK_PRODUCT"] ?>" class="works__card-descr"><?=$arItem["NAME"]?></a>
-                            <a href="<?= $arItem["LINK_PRODUCT"] ?>" class="works__card-price"><?=$arItem["PROPERTIES"]["PRICE"]["VALUE"]?></a>
-                            <button class="works__card-addToBasket">Добавить модель в корзину</button>
+                            <?if($arItem["PROPERTIES"]["PDF_FILE"]["VALUE"]):?>
+                                <img src="/design/icons/works_icon.svg" class="works__icon" alt="">
+                            <?endif;?>
+                        </a>
+                        <a href="<?=$arItem["DETAIL_PAGE_URL"]?>" class="works__card-modal"><?=$arResult["SECTIONS_LIST"][$sectionID]?></a>
+                        <a href="<?=$arItem["DETAIL_PAGE_URL"]?>" class="works__card-descr"><?=$arItem["NAME"]?></a>
+                        <a href="<?=$arItem["DETAIL_PAGE_URL"]?>" class="works__card-price"><?=number_format($arItem["PROPERTIES"]["PRICE"]["VALUE"], 0, ',', ' ')?> ₽</a>
+                        <button class="works__card-addToBasket add2Cart" data-itemid="<?=$arItem["ID"]?>" data-price="<?=$arItem["PROPERTIES"]["PRICE"]["VALUE"]?>">Добавить модель в корзину</button>
                     </div>
                 </div>
             <? endforeach; ?>
         </div>
-
     </div>
-
 </section>
-
