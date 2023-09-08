5f1844a2dce81f0b08f1a389cdf96eb2e0b4ceb3
pvashooot <pvashooot@rambler.ru>
2023-09-08 17:56:46 +0300

    Подключил себя к скриптам

--- bitrix/templates/stavbim/footer.php
+++ bitrix/templates/stavbim/footer.php
@@ -66,5 +66,6 @@
 <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
 <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
 <script src="/design/js/script.js"></script>
+<script src="/design/js/pvashooot.js"></script>
 </body>
 </html>
