5e196e152795e6c85a39d5bbfdd9ae59aae9bf56
Anastasia Savinova <nsty.nirvana@gmail.com>
2023-10-18 15:15:33 +0300

    починить 404ю

--- 404.php
+++ 404.php
@@ -1,16 +1,3 @@
-<?/*
-
-include_once($_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/main/include/urlrewrite.php');
-
-CHTTP::SetStatus("404 Not Found");
-@define("ERROR_404","Y");
-
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/templates/header.php");
-
-$APPLICATION->SetTitle("Страница не найдена");
-*/
-?>
-
 <?
 include_once($_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/main/include/urlrewrite.php');
 
@@ -27,7 +14,7 @@ $APPLICATION->AddChainItem("404");
 <section class="not-found">
     <div class="container">
         <div class="not-found__wrapper">
-            <img src="design/img/404.png" alt="" class="not-found__img">
+            <img src="/design/img/404.png" alt="" class="not-found__img">
             <a href="/" class="not-found__btn">Вернуться на главную</a>
         </div>
     </div>
